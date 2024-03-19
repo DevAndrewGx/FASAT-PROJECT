b74a467035d2d4f243bb1215c50b0350d403e924
AndrewGx <128672267+DevAndrewGx@users.noreply.github.com>
2024-03-19 16:13:51 -0500

    Adding new features like icons, styles and new content 03/19

--- views/js/app.js
+++ views/js/app.js
@@ -14,3 +14,19 @@ sidebarItems.forEach((element) => {
         activeItem = element;
     });
 });
+
+
+const navegationItems = document.querySelectorAll('main .nav-sections nav ul li');
+
+let activeNavItem = navegationItems[0];
+
+navegationItems.forEach((element) => {
+    element.addEventListener("click", () => {
+        if (activeNavItem) {
+            activeNavItem.removeAttribute("id");
+        }
+
+        element.setAttribute("id", "active");
+        activeItem = element;
+    });
+})
