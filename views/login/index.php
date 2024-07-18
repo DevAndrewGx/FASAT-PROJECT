a6d23ea3aff553f55fb3eecb846ad9008ec5c882
AndrewGx <128672267+DevAndrewGx@users.noreply.github.com>
2024-07-17 22:31:10 -0500

    Solving merge problems

--- views/login/index.php
+++ views/login/index.php
@@ -1,4 +1,3 @@
-<!-- este archivo va ser siempre el primero en que se cargue cuando accedamos al controlador del login -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,9 +13,10 @@
         <div class="login-container">
             <div class="logo-container">
                 <a href="../html/admin/adminDashboard.php"><img src="<?php echo constant('URL'); ?>public/imgs/LOGOf.png" alt="Logo" class="logo"></a>
-                <h2>Inicia sesión</h2>
+
             </div>
-            <form method="POST" action="<?php echo constant("URL")?>login/authenticate">
+            <form id="formLogin" method="POST" action="<?php echo constant("URL") ?>login/authenticate">
+                <h2>Inicia sesión</h2>
                 <div class="input-container">
                     <input type="text" id="username" placeholder="" name="correo" required>
                     <label for="username">Correo</label>
@@ -26,11 +26,25 @@
                     <label for="password">Password</label>
                 </div>
                 <button type="submit" id="btn-login">Login</button>
+                <p class="bottom-text">¿Olvidaste tu contraseña? Haz click <a href="#" id="btn-olvido-pass">aquí</a>.</p>
             </form>
-            <p class="bottom-text">Eres nuevo? Deseas iniciar prueba? Haz click <a href="signup.html">aquí</a>.</p>
+            <form id="formOlvidoPass" method="POST" action="<?php echo constant("URL")?>" class="forget-form" style="display: none;">
+                <h2>Olvidaste tu contraseña?</h2>
+                <div class="input-container">
+                    <input type="email" id="txtEmailReset" placeholder="Email" required>
+                    <label for="txtEmailReset">Email</label>
+                </div>
+                <button type="submit" id="btn-reset" class="btn-reset">Enviar correo</button>
+                <p class="bottom-text">Volver a <a href="#" id="btn-back-login">Iniciar sesión</a>.</p>
+            </form>
+
         </div>
     </div>
 
+    <!-- JavaScript para el efecto de volteo y manejo del formulario adicional -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <script src="<?php echo constant('URL'); ?>public/js/functions_login.js"></script>
 </body>
 
 </html>
